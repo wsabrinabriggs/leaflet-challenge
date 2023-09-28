f6dc8d31ad462466d7ff7ae6714e50a04f2c8907
Whitney Sabrina <w.sabrinabriggs@gmail.com>
2023-09-27 21:15:26 -0400

    updates

--- Challenge_15-Starter_Code/Starter_Code/static/js/logic.js
+++ Challenge_15-Starter_Code/Starter_Code/static/js/logic.js
@@ -1,7 +1,28 @@
+//Store the API endpoint as queryUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+//Perform a GET request to the query URL/
+d3.json(queryUrl).then(functon (data) {
+  //After getting a response, send the data.features object to the creaeFeatures function.
+  createFeatures(data.features);
+});
+
+// function createFeatures(earthquakesData) {
+//   function onEachFeature(feature, layer) {
+//     layer.bindPopup('<h3>${feature.properties.mag}</h3>');
+//   }
+
+//   let earthquakes = L.geoJSON(earthquakesData, {
+//     onEachFeature: onEachFeature
+//   });
+
+//   createImageBitmap(earthquakes);
+// }
+
 // Creating the map object
 let myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
+    center: [38.8026, -116.419438],
+    zoom: 5
   });
   
   // Adding the tile layer
@@ -9,15 +30,17 @@ let myMap = L.map("map", {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
   
-  // Use this link to get the GeoJSON data.
-  let link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
+  // // Use this link to get the GeoJSON data.
+  // let link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
   
-  // Getting our GeoJSON data
-  d3.json(link).then(function(data) {
-    // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data).addTo(myMap);
-  });
+  // // Getting our GeoJSON data
+  // d3.json(link).then(function(data) {
+  //   // Creating a GeoJSON layer with the retrieved data
+  //   L.geoJson(data).addTo(myMap);
+  // });
   
+
+
 
 // //Store the endpoint as queryUrl
 // let queryUrl = https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
