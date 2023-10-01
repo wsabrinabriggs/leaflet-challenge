cccfdd5c36bd41d85c036912c1edf4a41063473f
Whitney Sabrina <w.sabrinabriggs@gmail.com>
2023-09-30 22:28:47 -0400

    updates

--- Challenge_15-Starter_Code/Starter_Code/static/js/newlogic.js
+++ Challenge_15-Starter_Code/Starter_Code/static/js/newlogic.js
@@ -6,6 +6,19 @@ d3.json(queryUrl).then(function (data) {
   createFeatures(data.features);
 });
 
+// Create markers whose size increases with magnitude and color with depth
+function createMarker(feature, latlng) {
+  return L.circleMarker(latlng, {
+      radius: markerSize(feature.properties.mag),
+      fillColor: markerColor(feature.geometry.coordinates[2]),
+      color:"#000",
+      weight: 0.5,
+      opacity: 0.5,
+      fillOpacity: 1
+  });
+}
+
+
 function createFeatures(earthquakeData) {
 
     // Define a function that we want to run once for each feature in the features array.
@@ -17,12 +30,30 @@ function createFeatures(earthquakeData) {
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
     // Run the onEachFeature function once for each piece of data in the array.
     let earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
+      onEachFeature: onEachFeature,
+      pointToLayer: createMarker
     });
   
     // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
+  // 
+  createMap(earthquakes);
   }
+
+// let heatArray = [];
+
+// for (let i =0; i<features.length; i++) {
+//   let location = features[i].geometry;
+//   if (location) {
+//     //console.log(location);
+//     heatArray.push([location.coordinates[1], location.coordinates[0]]);
+//   }
+// }
+
+// let heat = L.heatLayer(heatArray, {
+//   radius: 20,
+//   blur: 35
+// }).addTo(myMap);
+
 
   function createMap(earthquakes) {
 
@@ -63,6 +94,21 @@ function createFeatures(earthquakeData) {
   }).addTo(myMap);
 
 }
+
+//increase size of marker based on magnitude
+function markerSize (magnitude) {
+  return magnitude * 5;
+}
+
+//change marker color based on depth
+function markerColor(depth) {
+  return depth > 90 ? '#d73027' :
+  depth > 70 ? '#fc8d59' :
+  depth > 50 ? '#fee08b' :
+  depth > 30 ? '#d9ef8b' :
+  depth > 10 ? '#91cf60' :
+               '#1a9850' ;          
+} 
 
 // Looping through the cities array, create one marker for each city, bind a popup 
 //for (let i =0; i < )
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Hello and thank you for reading my code.
 
 Please use newlogic.js as my JavaScript file for this challenge. There is no part 2 included in my challenge as I was unable to complete part 1 in its entirety at this time. My next steps to update this challenge include learning how to plot my data using circles with Leaflet and how to include popups as well as a legend.
 
+I've started using parts of a code from jonkwiatkowski on Github. The link to his code is the following: https://github.com/jonkwiatkowski/Leaflet/blob/main/Leaflet-Part-1/static/js/logic.js#L76
